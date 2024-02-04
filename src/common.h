edcd3b7a82a3bea0e4767106b9ec3b11b0caa076
Andy <azogg65@bluewin.ch>
2024-02-04 07:46:19 +0100

    support for multiple n2k sources

--- src/common.h
+++ src/common.h
@@ -14,9 +14,12 @@
 #include "N2kMsg.h"
 #include "N2kTypes.h"
 
+#define TemperaturDevice 0
+#define AlarmDevice 1
+
 extern uint8_t gN2KInstance;
 extern uint8_t gN2KSID;
-extern uint8_t gN2KSource;
+extern uint8_t gN2KSource[];
 
 extern int8_t gDeviceCount;
 
