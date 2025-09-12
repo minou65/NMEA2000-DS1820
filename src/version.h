df8f0e7a6814f037ebd7f291629ffc3838896842
Andy <azogg65@bluewin.ch>
2025-09-12 13:37:36 +0200

    Update version to 1.2.0 with build number 240 and correct timestamp

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 1
-#define VERSION_PATCH 15
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 239
+#define VERSION_BUILD 240
 #define VERSION_DATE "2025-09-12"
-#define VERSION_TIME "13:09:05"
-#define VERSION "1.1.15.239"
-#define VERSION_STR "1.1.15.239 (2025-09-12 13:09:05)"
+#define VERSION_TIME "13:33:40"
+#define VERSION "1.1.15.240"
+#define VERSION_STR "1.1.15.240 (2025-09-12 13:33:40)"
