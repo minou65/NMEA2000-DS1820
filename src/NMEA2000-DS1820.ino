bd50d693185b519efd16921dc224e77de6a8c2cd
Andy <azogg65@bluewin.ch>
2026-05-31 19:08:20 +0200

    Fix: initialize reboot manager and log reboot count and reason

--- src/NMEA2000-DS1820.ino
+++ src/NMEA2000-DS1820.ino
@@ -19,7 +19,6 @@
 #include <esp_mac.h>
 #include <esp_task_wdt.h>
 #include <Preferences.h>
-#include <esp_system.h>
 
 #include <N2kAlertTypes.h>
 #include <NMEA2000_CAN.h>
@@ -30,9 +29,6 @@
 #include "version.h"
 
 char Version[] = VERSION_STR; // Manufacturer's Software version code
-
-// Watchdog timeout in seconds
-#define WDT_TIMEOUT 10
 
 uint8_t gN2KSource[] = { 22, 23, 24, 25 };
 uint8_t gN2KInstance = 1;
@@ -75,44 +71,6 @@ const unsigned long TemperaturDeviceMessages[] PROGMEM = {
     126985L, // Alert text
     0 
 };
-
-void logRebootReason() {
-    Preferences prefs_;
-    prefs_.begin("system", false);
-
-    // increment reboot count
-    uint32_t rebootCount_ = prefs_.getUInt("reboots", 0);
-    rebootCount_++;
-    prefs_.putUInt("reboots", rebootCount_);
-
-    // store last reboot reason
-    esp_reset_reason_t reason_ = esp_reset_reason();
-    prefs_.putUInt("last_reason", (uint32_t)reason_);
-
-    prefs_.end();
-
-    Serial.print("Reboot reason: ");
-    switch (reason_) {
-    case ESP_RST_UNKNOWN:    Serial.println("Unknown"); break;
-    case ESP_RST_POWERON:    Serial.println("Power on"); break;
-    case ESP_RST_EXT:        Serial.println("External reset"); break;
-    case ESP_RST_SW:         Serial.println("Software reset"); break;
-    case ESP_RST_PANIC:      Serial.println("Panic reset"); break;
-    case ESP_RST_INT_WDT:    Serial.println("Interrupt watchdog reset"); break;
-    case ESP_RST_TASK_WDT:   Serial.println("Task watchdog reset"); break;
-    case ESP_RST_WDT:        Serial.println("Other watchdog reset"); break;
-    case ESP_RST_DEEPSLEEP:  Serial.println("Deep sleep reset"); break;
-    case ESP_RST_BROWNOUT:   Serial.println("Brownout reset"); break;
-    case ESP_RST_SDIO:       Serial.println("SDIO reset"); break;
-    case ESP_RST_USB:        Serial.println("USB reset"); break;
-    case ESP_RST_JTAG:       Serial.println("JTAG reset"); break;
-    case ESP_RST_EFUSE:      Serial.println("EFUSE reset"); break;
-    case ESP_RST_PWR_GLITCH: Serial.println("Power glitch reset"); break;
-    case ESP_RST_CPU_LOCKUP: Serial.println("CPU lockup reset"); break;
-    default:                Serial.println("Unknown reason code " + String((uint32_t)reason_)); break;
-    }
-    Serial.println("Reboot count: " + String(rebootCount_));
-}
 
 void OnN2kOpen() {
     Sensor* sensor_ = &Sensor1;
@@ -166,7 +124,9 @@ void setup() {
     }
     Serial.printf("Firmware version:%s\n", Version);
 
-    RebootManager::logRebootReason();
+    RebootManager::begin();
+    Serial.printf("Reboot count: %d\n", RebootManager::getRebootCount());
+    Serial.printf("Last reboot reason: %s\n", RebootManager::getLastRebootReasonText().c_str());
 
     randomSeed(esp_random());
 
